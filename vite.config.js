29c2beed92e7c6c8cd25003c00ccb77c3df37379
Daniel Sarmiento <daniel.sarmiento3@dafiti.com.co>
2022-08-29 15:28:49 -0500

    add google tag manager

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,19 @@ import {
 } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Icons from 'unplugin-icons/vite'
+import ViteRadar from 'vite-plugin-radar'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Icons(),
+    ViteRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-3EVNBKNWPN',
+      },
+    })
   ],
   define: {
     'process.env': process.env,
